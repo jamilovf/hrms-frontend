1895d0dca4aa5db5c3e4437ea4c356af4c892b43
Fuad Jamilov <fdjmlv666@gmail.com>
2021-07-31 15:18:14 +0400

    Some updates on AdvertisementFilter

--- src/pages/advertisements/AdvertisementFilter.jsx
+++ src/pages/advertisements/AdvertisementFilter.jsx
@@ -2,20 +2,33 @@ import React, { useState,useEffect } from "react";
 import "./AdvertisementFilter.css"
 import { Form, Dropdown, Checkbox, Button, Menu } from 'semantic-ui-react'
 import JobPositionService from "../../services/jobPositionService";
+import CompanyService from "../../services/companyService";
 
 export default function AdvertisementFilter() {
 
   const [jobPositions, setJobPositions] = useState([]);
 
+  const [companies, setCompanies] = useState([]);
+  
+
   useEffect(() => {
     let jobPositionService = new JobPositionService()
     jobPositionService.getAll().then(result => setJobPositions(result.data.data))
+
+    let companyService = new CompanyService();
+    companyService.getAll().then(result => setCompanies(result.data.data))
   })
 
   const jobPositionOptions = jobPositions.map((jobPosition) => ({
     key: jobPosition.id,
     text: jobPosition.position,
     value: jobPosition.id,  
+  }))
+
+  const companyOptions = companies.map((company) => ({
+    key: company.id,
+    text: company.companyName,
+    value: company.id,  
   }))
 
   return (
@@ -30,7 +43,7 @@ export default function AdvertisementFilter() {
         <Dropdown placeholder='Job Position' search selection options={jobPositionOptions}/>
         </Form.Field>
         <Form.Field inline>
-        <Dropdown placeholder='Company' search selection/>
+        <Dropdown placeholder='Company' search selection options={companyOptions}/>
         </Form.Field>
         <Form.Field inline>
         <Checkbox label='Part-time' />
